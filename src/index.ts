cf9b454121aa0e89dfbd55fc6453262afc363df5
James Conner <jamesbconner@gmail.com>
2021-10-24 12:00:19 -0400

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { IThemeManager } from '@jupyterlab/apputils';
+
+/**
+ * Initialization data for the @jamcon/jupyterlab-theme-umich extension.
+ */
+const extension: JupyterFrontEndPlugin<void> = {
+  id: '@jamcon/jupyterlab-theme-umich',
+  requires: [IThemeManager],
+  autoStart: true,
+  activate: (app: JupyterFrontEnd, manager: IThemeManager) => {
+    console.log('JupyterLab extension @jamcon/jupyterlab-theme-umich is activated!');
+    const style = '@jamcon/jupyterlab-theme-umich/index.css';
+
+    manager.register({
+      name: 'Go Blue!',
+      isLight: true,
+      load: () => manager.loadCSS(style),
+      unload: () => Promise.resolve(undefined)
+    });
+  }
+};
+
+export default extension;
